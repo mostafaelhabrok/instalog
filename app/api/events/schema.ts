ece18b54be84cbee2d3be1245158d4cb39d06efa
mostafa <mostafa.elhabrok@gmail.com>
2024-03-13 00:46:53 +0200

    apply event object structure

--- app/api/events/schema.ts
+++ app/api/events/schema.ts
@@ -6,16 +6,20 @@ const schema = z.object({
     actor_name: z.string().min(1),
     actor_email: z.string().email().min(1),
     group: z.string().min(1),
-    action_id: z.string().min(1),
-    action_name: z.string().min(1),
+    action: z.object({
+        id: z.string().min(1),
+        name: z.string().min(1),
+    }),
     target_id: z.string().min(1),
     target_name: z.string().min(1),
     target_email: z.string().email().min(1),
     location: z.string().min(1),
     occurred_at: z.string().datetime().default((new Date()).toISOString()),
-    meta_redirect: z.string().min(1),
-    meta_description: z.string().min(1),
-    meta_x_request_id: z.string().min(1)
+    metadata: z.object({
+        redirect: z.string().min(1),
+        description: z.string().min(1),
+        x_request_id: z.string().min(1)
+    }),
 });
 
 export default schema;
