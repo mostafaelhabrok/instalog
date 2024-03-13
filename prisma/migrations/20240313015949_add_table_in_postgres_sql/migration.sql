c46625495d0984c80a344d6b8aa3f644a4e58321
mostafa <mostafa.elhabrok@gmail.com>
2024-03-13 04:02:36 +0200

    migrate to live postgres sql

---
+++ prisma/migrations/20240313015949_add_table_in_postgres_sql/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "event_id" TEXT NOT NULL,
+    "object" TEXT DEFAULT 'event',
+    "actor_id" TEXT NOT NULL,
+    "actor_name" TEXT NOT NULL,
+    "actor_email" TEXT NOT NULL,
+    "group" TEXT NOT NULL,
+    "action_id" TEXT NOT NULL,
+    "action_object" TEXT DEFAULT 'event_action',
+    "action_name" TEXT NOT NULL,
+    "target_id" TEXT NOT NULL,
+    "target_name" TEXT NOT NULL,
+    "target_email" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "occurred_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "meta_redirect" TEXT NOT NULL,
+    "meta_description" TEXT NOT NULL,
+    "meta_x_request_id" TEXT NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
